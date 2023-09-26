b980ee7c49da5d244d4135391768564a09cd000f
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-09-26 13:38:37 +0000

    commited from Codespace

--- server.js
+++ server.js
@@ -1,21 +1,58 @@
 const express = require('express');
-const path = require('path');
+const multer = require('multer');
+const { PrismaClient } = require('./generated/client'); // Adjust the path as needed
 
 const app = express();
+const prisma = new PrismaClient();
+
 const PORT = process.env.PORT || 3000;
 
-// Define the path to your HTML file
-const htmlFilePath = path.join(__dirname, 'public', 'import.html');
+// Middleware for parsing multipart/form-data (file upload)
+const storage = multer.memoryStorage(); // Store the file in memory
+const upload = multer({ storage: storage });
 
-// Serve static files (e.g., CSS, images) from the 'public' directory
+app.use(express.json());
+
+// Serve your HTML file
 app.use(express.static('public'));
 
-// Define a route to serve the HTML file
-app.get('/', (req, res) => {
-    res.sendFile(htmlFilePath);
+app.post('/upload-excel', upload.single('excelFile'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    // Assuming the uploaded file is in XLSX format
+    const xlsxFile = req.file.buffer;
+
+    // Parse the XLSX file (you'll need to install the 'xlsx' package)
+    const XLSX = require('xlsx');
+    const workbook = XLSX.read(xlsxFile);
+
+    // Assuming you want to process the first sheet
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    // Convert XLSX data to an array of objects (you might need to adjust this based on your Excel structure)
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    // Store the data in the database using Prisma
+    await prisma.user.createMany({
+      data: data.map((item) => ({
+        Name: item.Name,
+        Email: item.Email,
+        Program: item.Program,
+        ImageUrl: item.ImageUrl || null,
+      })),
+    });
+
+    res.status(200).json({ message: 'Data imported successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing the file' });
+  }
 });
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
