fbab1ed272caef32f5bf0ec5043ae4f7b73aa7dc
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-10-02 15:49:34 +0000

    server /index file changed to /landing file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 // Serve the HTML form
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
 // Serve the register.html page
