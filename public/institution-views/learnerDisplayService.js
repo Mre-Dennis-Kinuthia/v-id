62ea9baa6639e5fd197c51c1b5c425b34c2803b0
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-10-08 20:16:31 +0000

    init

--- public/institution-views/learnerDisplayService.js
+++ public/institution-views/learnerDisplayService.js
@@ -12,25 +12,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Make a request to search for learners by name
             const response = await fetch(`/search?name=${name}`);
+
             if (response.ok) {
-                const learner = await response.json();
+                const contentType = response.headers.get('content-type');
+                if (contentType && contentType.includes('application/json')) {
+                    // Parse the response as JSON
+                    const learner = await response.json();
 
-                // Clear the learner display container
-                learnerDisplayContainer.innerHTML = '';
+                    // Clear the learner display container
+                    learnerDisplayContainer.innerHTML = '';
 
-                if (learner) {
-                    // Display the learner's profile
-                    const learnerCard = document.createElement('div');
-                    learnerCard.classList.add('learner-card');
-                    learnerCard.innerHTML = `
-                        <h2>${learner.Name}</h2>
-                        <p>Email: ${learner.learnerEmail}</p>
-                        <p>Program: ${learner.Program}</p>
-                    `;
-                    learnerDisplayContainer.appendChild(learnerCard);
+                    if (learner) {
+                        // Display the learner's profile
+                        const learnerCard = document.createElement('div');
+                        learnerCard.classList.add('learner-card');
+                        learnerCard.innerHTML = `
+                            <h2>${learner.Name}</h2>
+                            <p>Email: ${learner.learnerEmail}</p>
+                            <p>Program: ${learner.Program}</p>
+                        `;
+                        learnerDisplayContainer.appendChild(learnerCard);
+                    } else {
+                        // Display a message when no learner is found
+                        learnerDisplayContainer.innerHTML = '<p>No learner found with that name.</p>';
+                    }
                 } else {
-                    // Display a message when no learner is found
-                    learnerDisplayContainer.innerHTML = '<p>No learner found with that name.</p>';
+                    console.error('Response is not JSON:', contentType);
+                    // Handle the case where the response is not JSON
+                    // You can display an error message or handle it as needed.
                 }
             } else {
                 console.error('Failed to fetch learner data:', response.statusText);
