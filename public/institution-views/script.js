91c4d2833eb82bc6223c92ef372f42236505a871
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-10-08 16:51:11 +0000

    init

--- public/institution-views/script.js
+++ public/institution-views/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const view3 = document.getElementById('view3');
     const view4 = document.getElementById('view4');
     const sidebar = document.querySelector('.vertical-sidebar');
-    const content = document.querySelector('.content');
+    const content = document.querySelector('main'); // Updated selector
     const learnerContainer = document.getElementById('learnerContainer');
 
     const toggleSidebarBtn = document.getElementById('toggleSidebar');
@@ -17,53 +17,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     toggleSidebarBtn.addEventListener('click', () => {
         nav.classList.toggle('collapsed');
+        content.classList.toggle('expanded');
     });
 
     // By default, you might want the sidebar to be collapsed on page load
     nav.classList.add('collapsed');
 
-
-    /* // Function to create a learner card
-    function createLearnerCard(learner) {
-        const card = document.createElement('div');
-        card.classList.add('learner-card');
-
-        const name = document.createElement('h2');
-        name.textContent = learner.name; // Assuming the learner object has a 'name' property
-
-        const program = document.createElement('p');
-        program.textContent = 'Program: ' + learner.program;
-        // Assuming the learner object has a 'program' property
-        // You can add more learner details here
-        card.appendChild(name);
-        card.appendChild(program);
-
-        return card;
-    }
-
-Function to fetch learner data and display cards
-    async function displayLearners() {
-        try {
-            const response = await fetch('/api/learners'); // Replace with your API endpoint
-            const learners = await response.json();
-
-            // Clear existing learner cards
-            learnerContainer.innerHTML = '';
-
-            // Create and append learner cards
-            learners.forEach((learner) => {
-                const learnerCard = createLearnerCard(learner);
-                learnerContainer.appendChild(learnerCard);
-            })
-        } catch (error) {
-            console.error('Error fetching and displaying learners:', error);
-        }
-    }
-*/
     btnView1.addEventListener('click', () => {
         hideAllViews();
         view1.style.display = 'block';
-        displayLearners(); 
         // Fetch and display learners when the "Dashboard" button is clicked
     });
 
@@ -82,18 +44,10 @@ Function to fetch learner data and display cards
         view4.style.display = 'block';
     });
 
-
     function hideAllViews() {
         view1.style.display = 'none';
         view2.style.display = 'none';
         view3.style.display = 'none';
         view4.style.display = 'none';
     }
-
-    const toggleSidebar = document.getElementById('toggleSidebar');
-
-    toggleSidebar.addEventListener('click', () => {
-        sidebar.classList.toggle('collapsed');
-        content.classList.toggle('expanded');
-    });
 });
